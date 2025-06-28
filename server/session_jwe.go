5037c530c692695048221982ff116c54f02e6d33
Kenneth Lee <kennethlee@fortinet.com>
2025-06-27 21:40:27 -0400

    feat: rollback rollback golang to 1.24 and upgrades to go-jose v4

--- server/session_jwe.go
+++ server/session_jwe.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/crewjam/saml"
 	"github.com/crewjam/saml/samlsp"
-	"github.com/go-jose/go-jose/v3"
+	"github.com/go-jose/go-jose/v4"
 )
 
 // JWESessionCodec wraps a standard SessionCodec and applies JWE encryption to protect sensitive attributes
@@ -31,15 +31,15 @@ func NewJWESessionCodec(wrapped samlsp.SessionCodec) (samlsp.SessionCodec, error
 		return nil, fmt.Errorf("failed to create jwe encrypter: %w", err)
 	}
 
-	return JWESessionCodec{wrapped: wrapped, encrypter: encrypter, privateKey: privateKey}, nil
+	return &JWESessionCodec{wrapped: wrapped, encrypter: encrypter, privateKey: privateKey}, nil
 }
 
-func (c JWESessionCodec) New(assertion *saml.Assertion) (samlsp.Session, error) {
+func (c *JWESessionCodec) New(assertion *saml.Assertion) (samlsp.Session, error) {
 	return c.wrapped.New(assertion)
 }
 
 // Encode first creates a signed JWT (JWS) using the wrapped codec, and then encrypts the entire JWS payload using JWE.
-func (c JWESessionCodec) Encode(s samlsp.Session) (string, error) {
+func (c *JWESessionCodec) Encode(s samlsp.Session) (string, error) {
 	// get the signed JWT (JWS) from the underlying codec
 	signed, err := c.wrapped.Encode(s)
 	if err != nil {
@@ -57,9 +57,9 @@ func (c JWESessionCodec) Encode(s samlsp.Session) (string, error) {
 
 // Decode first decrypts the JWE payload to get the signed JWT (JWS), and then uses the wrapped codec to decode and
 // validate the JWS
-func (c JWESessionCodec) Decode(encrypted string) (samlsp.Session, error) {
-	// parse the JWE token
-	jwe, err := jose.ParseEncrypted(encrypted)
+func (c *JWESessionCodec) Decode(encrypted string) (samlsp.Session, error) {
+	// parse the JWE token (possible to parameterize jose.ContentEncryption and jose.KeyAlgorithm)
+	jwe, err := jose.ParseEncrypted(encrypted, []jose.KeyAlgorithm{jose.RSA_OAEP}, []jose.ContentEncryption{jose.A128GCM})
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse jwe token: %w", err)
 	}
